711d21daaa51d80123f9344c57a1f47f96c46742
akshay-bhor <akbhor50@gmail.com>
2024-09-22 00:07:26 +0530

    homepage

--- client/modules/Home/Hero.tsx
+++ client/modules/Home/Hero.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { OrbitControls, Text, useAnimations, useGLTF } from "@react-three/drei";
+import { Environment, OrbitControls, Text, useAnimations, useGLTF } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { RefObject, useEffect, useRef, useState } from "react";
 import { Group, Mesh, Vector3 } from "three";
@@ -7,7 +7,7 @@ import { Group, Mesh, Vector3 } from "three";
 const Hero = () => {
     return (
         <div className="relative">
-            <div className="flex flex-col md:flex-row items-center justify-between h-[calc(100vh-75px)] z-10 px-4 md:px-8">
+            <div className="relative flex flex-col md:flex-row items-center justify-between h-[calc(100vh-75px)] z-10 px-4 md:px-8">
                 <div className="flex items-center justify-center w-full h-1/2 md:h-full md:w-1/2">
                     <div className="flex flex-col items-center justify-center md:-mt-32">
                         <h1 className="text-6xl md:text-8xl font-bold mb-4">Akshay Bhor</h1>
@@ -23,12 +23,18 @@ const Hero = () => {
                     </Canvas>
                 </div>
             </div>
-            <div className="z-1 absolute left-0 right-0 top-0 bottom-0">
+            <div className="z-0 absolute left-0 right-0 top-0 bottom-0">
                 <Canvas camera={{ position: [0, 0, 5] }}>
-                    <ambientLight intensity={2} />
+                    <Environment preset="forest" />
                     <BackgroundCubes />
-                    <OrbitControls />
-                    <axesHelper />
+                    <spotLight
+                        position={[0, 5, 5]}
+                        intensity={50}
+                        angle={Math.PI / 2}
+                        penumbra={0.4}
+                        decay={0.6}
+                        castShadow
+                    />
                 </Canvas>
             </div>
         </div>
@@ -61,7 +67,7 @@ const BackgroundCubes = () => {
 
     useEffect(() => {
         const initPos = [...Array(20)].map(() => ({
-            position: new Vector3(Math.random() * 11 - 5, 10, 0),
+            position: new Vector3(Math.random() * 11 - 5, 10, Math.random() * 5 - 5),
             velocity: new Vector3(0, Math.random() * 0.02, 0),
         }));
 
@@ -87,6 +93,8 @@ const BackgroundCubes = () => {
                     ref={(el) => {
                         if (el) cubesRef.current[i] = el;
                     }}
+                    castShadow
+                    receiveShadow
                 >
                     <boxGeometry args={[1, 1, 0.1]} />
                     <meshPhysicalMaterial
@@ -94,7 +102,7 @@ const BackgroundCubes = () => {
                         opacity={0.9}
                         transmission={0.9}
                         roughness={0.1}
-                        metalness={0.4}
+                        metalness={0.6}
                     />
                     <Text position={[0, 0, 0.1]} fontSize={0.5} color="#ffffff" anchorX="center" anchorY="middle">
                         {"</>"}
